a4e4c4459f1bfbeb7c2e5774425f2dd8bd48bb13
Mael <mael.ogier.38@gmail.com>
2014-02-11 09:12:49 +0100

    change script sql

---
+++ site/src/Ghome/ContentBundle/Entity/TrameLearn.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Ghome\ContentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TrameLearn
+ *
+ * @ORM\Entity
+ */
+class TrameLearn
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="trame", type="string", length=10, nullable=false)
+     */
+    private $trame;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="idPhysiqueCapteur", type="integer", nullable=false)
+     */
+    private $idPhysiqueCapteur;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set trame
+     *
+     * @param string $trame
+     * @return TrameLearn
+     */
+    public function setTrame($trame)
+    {
+        $this->trame = $trame;
+
+        return $this;
+    }
+
+    /**
+     * Get trame
+     *
+     * @return string 
+     */
+    public function getTrame()
+    {
+        return $this->trame;
+    }
+
+    /**
+     * Set idPhysiqueCapteur
+     *
+     * @param integer $idPhysiqueCapteur
+     * @return TrameLearn
+     */
+    public function setIdPhysiqueCapteur($idPhysiqueCapteur)
+    {
+        $this->idPhysiqueCapteur = $idPhysiqueCapteur;
+
+        return $this;
+    }
+
+    /**
+     * Get idPhysiqueCapteur
+     *
+     * @return integer 
+     */
+    public function getIdPhysiqueCapteur()
+    {
+        return $this->idPhysiqueCapteur;
+    }
+}
