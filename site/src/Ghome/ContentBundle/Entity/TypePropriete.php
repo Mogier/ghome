ae337e5e2db765e5ef6bd313a872a0c7ad299e43
quentin <quentin@quentin-asus.(none)>
2014-02-24 21:07:50 +0100

    Correction majuscule sur Typepropriete

---
+++ site/src/Ghome/ContentBundle/Entity/TypePropriete.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Ghome\ContentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TypePropriete
+ *
+ * @ORM\Table(name="TypePropriete")
+ * @ORM\Entity
+ */
+class TypePropriete
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="label", type="string", length=32, nullable=true)
+     */
+    private $label;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return TypePropriete
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string 
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+}
